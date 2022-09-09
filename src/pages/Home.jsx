adbc7f3136850f226330199d06eff71cf18f390e
Benoit Mouly <bmouly@rouen-webmaster.com>
2022-09-09 19:54:34 +0200

    Responsive fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,8 @@ import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
 import Banner from '../components/Banner';
 import CardsGallery from '../components/CardsGallery';
-import '../styles/pages/Home.css'
+import '../styles/pages/Home.css';
 import Footer from '../components/Footer';
-// import Card from '../components/Card';
 
 const Home = () => {
     let bannerTitle = 'Chez vous, partout et ailleurs';
@@ -18,7 +17,6 @@ const Home = () => {
                 </div>
                 <Banner props={bannerTitle}/>
                 <CardsGallery/>
-            {/* <Card /> */}
             </div>
             <Footer />
         </div>
